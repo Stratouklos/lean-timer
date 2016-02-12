ce3e641325bfa6429297b5e353842d43a65b3d43
vid021 <efstratios.xakoustos@capitalone.com>
2016-02-12 12:47:15 -0500

    Functionally the first mvp is complete

--- app/topic/topic.js
+++ app/topic/topic.js
@@ -9,19 +9,46 @@ angular.module('myApp.topic', ['ngRoute', 'timer'])
   });
 }])
 
-.controller('topicCtrl', ['$scope', '$window', function($scope, $window) {
-  $scope.timerRunning = false;
 
-  $scope.startTimer = function (){
-    if ($scope.timerRunning === false) {
-      $scope.$broadcast('timer-start');
-      $scope.timerRunning = true;
+.controller('topicCtrl', ['$scope', '$window', function($scope, $window) {
+  $scope.discussing = false;
+  $scope.voteInProgress = false;
+
+  $scope.startTopic = function (){
+    if ($scope.discussing) {
+      console.log('Interrupting current topic');
+      $scope.$broadcast('timer-reset');
+      $scope.$broadcast('timer-set-countdown-seconds', 600);
     }
+    $scope.$broadcast('timer-start');
+    console.log('Starting a new topic');
+    $scope.discussing = true;
+    $scope.voteInProgress = false;
+    $scope.numberOfVotes = 0;
   };
 
-
   $scope.$on('timer-stopped', function (event, data){
-    $window.alert("Time's out!");
+    $window.alert("Time's out! Please vote to continue or not");
     console.log('Timer Stopped - data = ', data);
+    $scope.voteInProgress = true;
+    $scope.numberOfVotes++;
+    $scope.discussing = false;
+    $scope.$apply(function(){
+      $scope.showVote = true
+    });
   });
+
+  $scope.continueDiscussing = function () {
+    console.log('Continuing on the same topic for the last ', $scope.numberOfVotes);
+    if ($scope.numberOfVotes < 2) {
+      $scope.$broadcast('timer-set-countdown-seconds', 300);
+    } else {
+      $scope.$broadcast('timer-set-countdown-seconds', 120);
+    }
+    $scope.$broadcast('timer-start');
+  };
+
+  $scope.canVote = function () {
+      return !$scope.discussing && ($scope.numberOfVotes > 0)
+  }
 }]);
