1ae3eeed9f5eec1d2f973d10fc7d760c3d6ecf0b
Stratos Xakoustos <stratouklos@gmail.com>
2016-02-12 23:21:42 -0500

    formatting changes

--- app/leanTimer/leanTimerTest.js
+++ app/leanTimer/leanTimerTest.js
@@ -1,6 +1,6 @@
 'use strict';
 
-describe('myApp leanTimer module', function() {
+describe('myApp leanTimer module', function () {
 
     var scope, topicCtrl;
 
@@ -16,30 +16,30 @@ describe('myApp leanTimer module', function() {
     }));
 
 
-    describe('lean-timer controller', function(){
+    describe('lean-timer controller', function () {
 
-        it('should be defined and timer should be stopped', inject(function() {
+        it('should be defined and timer should be stopped', inject(function () {
             expect(topicCtrl).toBeDefined();
         }));
 
-        it('should be able to start the discussion by pushing a button', inject(function() {
+        it('should be able to start the discussion by pushing a button', inject(function () {
             scope.startTopic();
             expect(scope.$broadcast).toHaveBeenCalledWith('timer-start');
             expect(scope.$broadcast).toHaveBeenCalledWith('timer-set-countdown-seconds', 480);
         }));
 
-        it('should declare a continuation vote when the timer expires', inject(function() {
+        it('should declare a continuation vote when the timer expires', inject(function () {
             scope.startTopic();
             scope.$emit('timer-stopped');
             expect(scope.voting).toBe(true);
         }));
 
-        it('should start a four minute countdown on up votes', inject(function() {
+        it('should start a four minute countdown on up votes', inject(function () {
             scope.continueDiscussing(true);
             expect(scope.$broadcast).toHaveBeenCalledWith('timer-set-countdown-seconds', 240);
         }));
 
-        it('should start a two minute countdown on middle votes', inject(function() {
+        it('should start a two minute countdown on middle votes', inject(function () {
             scope.continueDiscussing();
             expect(scope.$broadcast).toHaveBeenCalledWith('timer-set-countdown-seconds', 120);
         }));
--- e2e-tests/scenarios.js
+++ e2e-tests/scenarios.js
@@ -1,32 +1,32 @@
 'use strict';
 
-describe('my app', function() {
+describe('my app', function () {
 
 
-  it('should automatically redirect to /lean-timer when location hash/fragment is empty', function() {
-    browser.get('index.html');
-    expect(browser.getLocationAbsUrl()).toMatch("/lean-timer");
-  });
-
-
-  describe('lean-timer', function() {
-
-    beforeEach(function() {
-      browser.get('index.html#/lean-timer');
+    it('should automatically redirect to /lean-timer when location hash/fragment is empty', function () {
+        browser.get('index.html');
+        expect(browser.getLocationAbsUrl()).toMatch("/lean-timer");
     });
 
 
-    it('should render lean-timer when user navigates to /lean-timer', function() {
-      expect(element.all(by.css('[ng-view] p')).first().getText()).
-        toMatch(/Pick a topic and discuss/);
+    describe('lean-timer', function () {
+
+        beforeEach(function () {
+            browser.get('index.html#/lean-timer');
+        });
+
+
+        it('should render lean-timer when user navigates to /lean-timer', function () {
+            expect(element.all(by.css('[ng-view] p')).first().getText()).
+                toMatch(/Pick a topic and discuss/);
+        });
+
+
+        it('should have a start button', function () {
+            expect(element.all(by.css('[ng-view] #start_button')).first().getText()).
+                toMatch(/Start/);
+        });
+
     });
-
-
-    it('should have a start button', function() {
-      expect(element.all(by.css('[ng-view] #start_button')).first().getText()).
-      toMatch(/Start/);
-    });
-
-  });
 
 });
