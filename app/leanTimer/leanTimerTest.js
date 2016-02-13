6acadfa84934b5960da0c69ee89ed2723ad316cf
Stratos Xakoustos <stratouklos@gmail.com>
2016-02-12 20:59:55 -0500

    Refactoring, and changing the way next timer duration is selected

--- app/leanTimer/leanTimerTest.js
+++ app/leanTimer/leanTimerTest.js
@@ -24,31 +24,26 @@ describe('myApp leanTimer module', function() {
         it('should be defined and timer should be stopped', inject(function() {
             var topicCtrl = createController();
             expect(topicCtrl).toBeDefined();
-            expect(scope.discussing).toBe(false);
         }));
 
         it('should be able to start the discussion by pushing a button', inject(function() {
             var topicCtrl = createController();
             scope.startTopic();
             expect(scope.$broadcast).toHaveBeenCalledWith('timer-start');
-            expect(scope.discussing).toBe(true);
         }));
 
         it('should be able to switch to a new discussion whenever the previous is depleted', inject(function() {
             createController();
             scope.startTopic();
             scope.startTopic();
-            expect(scope.$broadcast).toHaveBeenCalledWith('timer-reset');
             expect(scope.$broadcast).toHaveBeenCalledWith('timer-set-countdown-seconds', 600);
-            expect(scope.discussing).toBe(true);
         }));
 
         it('should declare a continuation vote when the timer expires', inject(function() {
             createController();
             scope.startTopic();
             scope.$emit('timer-stopped');
-            expect(scope.discussing).toBe(false);
-            expect(scope.voteInProgress).toBe(true);
+            expect(scope.voting).toBe(true);
         }));
 
 
