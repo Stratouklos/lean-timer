f1859b61a606993e614a23dca7295ab61012bf77
vid021 <efstratios.xakoustos@capitalone.com>
2016-02-16 12:24:58 -0500

    Naked yeoman

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name leanTimerApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the leanTimerApp
+ */
+angular.module('leanTimerApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
