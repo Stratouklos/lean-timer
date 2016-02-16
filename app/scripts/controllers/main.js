8c33176e9e674c4e65fb25479a5a571bac7b569b
Stratos Xakoustos <stratouklos@gmail.com>
2016-02-16 12:24:58 -0500

    Naked yeoman

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name leanTimerApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the leanTimerApp
+ */
+angular.module('leanTimerApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
